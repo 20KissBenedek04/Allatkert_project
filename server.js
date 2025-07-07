9739bf899c1d0f89d11289148300f7a798574f40
Benedek Kiss <kissbenedek007@gmail.com>
2025-07-07 19:26:31 +0200

    'keszserver'

--- server.js
+++ server.js
@@ -15,12 +15,61 @@ dbHandler.user.sync({ alter: true })
 server.use(express.json())
 server.use(cors())
 
+function Auth(){
+    return async (req,res,next) => {
+        const token = req.headers.authorization
+        try {
+            await JWT.verify(token, SECRETKEY)
+            next()
+        }
+        catch(error){
+            console.log('Hiba az auth-ban' + error)
+            res.json({'message':'sikertelen autentikáció'}).end()
+            return
+        }
+    }
+}
+
 server.get('/user', async (req, res) => {
     res.status(200).json(await dbHandler.user.findAll()).end()
 })
 
 server.get('/firstUser', async (req, res) => {
     res.status(200).json(await dbHandler.user.findOne()).end()
+})
+
+server.post('/login', async (req,res) => {
+    console.log(req.body)
+    const oneUser = await dbHandler.user.findOne({
+        where:{
+            userName: req.body.loginUser,
+            userPassword: req.body.loginPass
+        }
+    })
+    if(!oneUser){
+        res.status(401).json({'message':'Hibás felhasználó vagy jelszó'})
+        return
+    }
+    const token = await JWT.sign({userName: req.body.loginUser}, SECRETKEY, {expiresIn:'1h'})
+    res.json({'message':'Sikeres bejelentkezés', 'token': token}).end()
+})
+
+server.post('/registration', async (req,res) => {
+    console.log(req.body)
+    const oneUser = await dbHandler.user.findOne({
+        where:{
+            userName: req.body.regUser
+        }
+    })
+    if(oneUser){
+        res.status(409).json({'message':'Már létezik ilyen felhasználó'}).end()
+        return
+    }
+    await dbHandler.user.create({
+        userName: req.body.regUser,
+        userPassword: req.body.regPassword
+    })
+    res.status(201).json({'message':'Sikeres regisztráció'}).end()
 })
 
 server.post('/user', async (req, res) => {
